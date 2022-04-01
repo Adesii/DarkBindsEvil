f50a32501a855ce52f8ea7934376ee528b506de9
Adesi <kopietz123@gmail.com>
2022-04-01 02:43:37 +0200

    Refine UI Elements

--- code/UI/IngameHUD/Inventory.cs
+++ code/UI/IngameHUD/Inventory.cs
@@ -3,5 +3,23 @@ namespace DarkBinds.UI.IngameHUD;
 [UseTemplate, Library( "Inventory" )]
 public class Inventory : Panel
 {
+	public static Inventory Instance;
 
+	public Inventory()
+	{
+		Instance = this;
+	}
+	public static void Toggle()
+	{
+		Instance.SetClass( "hidden", !Instance.HasClass( "hidden" ) );
+	}
+	[Event.BuildInput]
+	public static void BuildInput( InputBuilder input )
+	{
+		if ( Instance?.HasClass( "hidden" ) ?? true )
+			return;
+
+		input.ClearButton( InputButton.Attack1 );
+		input.ClearButton( InputButton.Attack2 );
+	}
 }
--- code/UI/Loading/LoadingScreen.cs
+++ code/UI/Loading/LoadingScreen.cs
@@ -14,20 +14,25 @@ public class LoadingScreen : Panel
 	}
 
 	public Panel ProgressBar { get; set; }
+	float backgroundscroll;
 
 	public override void Tick()
 	{
 		base.Tick();
 
 		ProgressBar.Style.Width = Length.Fraction( World.WorldLoadingProgress );
+		backgroundscroll += RealTime.Delta * 64;
+		backgroundscroll %= 127;
+
+		ProgressBar.Style.BackgroundPositionX = Length.Pixels( (-128) + backgroundscroll );
 
 		if ( World.WorldLoadingProgress >= 1f )
 		{
 			SetClass( "hidden", true );
+			IngameRoot.Instance.SetClass( "hidden", false );
 			if ( ComputedStyle.Opacity == 0 )
 			{
 				Delete();
-				IngameRoot.Instance.SetClass( "hidden", false );
 			}
 		}
 	}
